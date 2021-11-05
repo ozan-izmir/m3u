5f5ad621d2574bd5205aa07de5371f951089d21f
liderkom <36478901+liderkom@users.noreply.github.com>
2021-11-05 19:20:12 +0300

    Update watt.php

--- watt.php
+++ watt.php
@@ -1,7 +1,7 @@
 #EXTM3U
-#EXTINF:-1,|FR|TF1 FHD
+#EXTINF:-1,|FR|TF1FHD
 http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2538
-#EXTINF:-1,|FR|FRANCE 2 FHD
+#EXTINF:-1,|FR|FRANCE2FHD
 http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2539
-#EXTINF:-1,|FR|FRANCE 3 FHD
+#EXTINF:-1,|FR|FRANCE3FHD
 http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2540
