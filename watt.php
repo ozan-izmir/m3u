328b6bf34767448ecdb6b4b2e159b5fb9acf3129
liderkom <36478901+liderkom@users.noreply.github.com>
2021-11-05 19:21:46 +0300

    Update watt.php

--- watt.php
+++ watt.php
@@ -5,3 +5,13 @@ http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2538
 http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2539
 #EXTINF:-1,|FR|FRANCE3FHD
 http://wagmaott.com:8080/mag00:1A:79:49:0F:A6/ubkyntGhbV/2540
+#EXTINF:-1,TR | KANAL D UHD
+http://ghost.dvrdns.org:2086/krXL50L00/88wQ004HY339/1242
+#EXTINF:-1,TRH | NTV HABER HD
+http://ghost.dvrdns.org:2086/krXL50L00/88wQ004HY339/1249
+#EXTINF:-1,TRH | CNN TURK HD
+http://ghost.dvrdns.org:2086/krXL50L00/88wQ004HY339/1250
+#EXTINF:-1,TRH | HABERTURK HD
+http://ghost.dvrdns.org:2086/krXL50L00/88wQ004HY339/25764
+#EXTINF:-1,TRH | HABER GLOBAL HD
+http://ghost.dvrdns.org:2086/krXL50L00/88wQ004HY339/1261
